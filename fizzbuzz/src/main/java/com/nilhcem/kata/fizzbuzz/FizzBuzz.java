001bf6f7569477e6a07357853a68b5bc719a7875
Gautier Nilhcem <nilhcem@gmail.com>
2014-10-05 11:56:25 +0200

    Add implementation for failing test

--- fizzbuzz/src/main/java/com/nilhcem/kata/fizzbuzz/FizzBuzz.java
+++ fizzbuzz/src/main/java/com/nilhcem/kata/fizzbuzz/FizzBuzz.java
@@ -1,10 +1,16 @@
 package com.nilhcem.kata.fizzbuzz;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FizzBuzz {
 
     public List<String> getNumbersInRange(int from, int to) {
-        return null;
+        List<String> result = new ArrayList<>(to - from + 1);
+
+        for (int i = from; i <= to; i++) {
+            result.add(Integer.toString(i));
+        }
+        return result;
     }
 }
