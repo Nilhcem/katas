201ecd3cdac8d8e249d68d5a9c8ebde487fd36b1
Gautier Nilhcem <nilhcem@gmail.com>
2014-10-10 23:38:36 +0200

    Add implementation for first failing test

--- 100doors/src/main/java/com/nilhcem/kata/hundreddoors/Room.java
+++ 100doors/src/main/java/com/nilhcem/kata/hundreddoors/Room.java
@@ -1,10 +1,22 @@
 package com.nilhcem.kata.hundreddoors;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Room {
 
+    private static final int NB_DOORS = 100;
+
+    private List<Door> doors;
+
+    public Room() {
+        doors = new ArrayList<>(NB_DOORS);
+        for (int i = 0; i < NB_DOORS; i++) {
+            doors.add(new Door());
+        }
+    }
+
     public List<Door> getAllDoors() {
-        return null;
+        return doors;
     }
 }
